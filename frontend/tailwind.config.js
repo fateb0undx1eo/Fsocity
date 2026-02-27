88e5f87a794b2c5a3940eb4406d6ee2d9a9adf84
fateb0undx1eo <explainersenpai@gmail.com>
2026-02-28 01:33:45 +0530

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["HelveticaNeueHeavy", "ui-sans-serif", "system-ui"],
+        serif: ["Playfair Display", "serif"],
+        display: ["Engry"],
+      },
+    },
+  },
+  plugins: [],
+};
